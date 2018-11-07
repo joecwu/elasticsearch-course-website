113ccbbab2f64bc5263a228989a6b07ce8c8c274
root <root@es-course.asia-east1-b.c.joecwu-221816.internal>
2018-11-07 17:07:10 +0000

    change run.sh absolute path

--- nginx/run.sh
+++ nginx/run.sh
@@ -1,1 +1,1 @@
-docker run --name nginx --restart=always -p 80:80 -v /opt/nginx/nginx.conf:/etc/nginx/nginx.conf:ro -v /opt/es-course:/usr/share/nginx/es-course -v /opt/nginx/logs:/var/nginx/logs -d nginx
+docker run --name nginx --restart=always -p 80:80 -v /mnt/elasticsearch-course-website/nginx/nginx.conf:/etc/nginx/nginx.conf:ro -v /mnt/elasticsearch-course-website/es-course:/usr/share/nginx/es-course -v /mnt/elasticsearch-course-website/nginx/logs:/var/nginx/logs -d nginx
